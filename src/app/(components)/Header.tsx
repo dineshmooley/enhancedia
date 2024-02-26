1adbb58404aa5019c3ba90ea1a9b6396540bdd57
Samsson R <samssonravi@gmail.com>
2024-02-26 16:03:31 +0530

    feat(dept): add cards, profile btn

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { MainNav } from "./ui/Main-nav";
 import { ModeToggle } from "./ui/mode-toggle";
 import { useSession } from "next-auth/react";
+import { Button } from "./ui/button";
 
 const Header = () => {
   const { data: session } = useSession();
@@ -17,7 +18,12 @@ const Header = () => {
                 LICET
               </Link>
               <MainNav className="mx-6" />
-              <ModeToggle />
+              <div className="flex items-center">
+                <ModeToggle />
+                <Button variant="outline" size="icon" className=" ml-3">
+                  S
+                </Button>
+              </div>
             </div>
           </div>
         </div>
