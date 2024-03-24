e0bac8858ad0f14b53bee8e0638189da44916d0f
dineshmooley <dineshkumar.24it@licet.ac.in>
2024-03-25 05:07:45 +0530

    test qns

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -22,48 +22,63 @@ import { Button } from "../(components)/ui/button";
 const QA = [
   {
     qno: 1,
-    qdif: "Easy",
-    q: "Who is the father of computer ?",
-    a: "Steve Jobs",
-    b: "Bill Gates",
-    c: "Charles Babbage",
-    d: "Crispin",
+    q: "I'd rather spend a free evening",
+    a: "reading or working alone on a project",
+    b: "working on a task with friends",
   },
   {
     qno: 2,
-    qdif: "Moderate",
-    q: "Who is the father of computer ?",
-    a: "Steve Jobs",
-    b: "Bill Gates",
-    c: "Charles Babbage",
-    d: "Crispin",
+    q: "Peoples form opinions about me so quickly",
+    a: "Hardly ever",
+    b: "Often",
   },
   {
     qno: 3,
-    qdif: "Easy",
-    q: "Who is the father of computer ?",
-    a: "Steve Jobs",
-    b: "Bill Gates",
-    c: "Charles Babbage",
-    d: "Crispin",
+    q: "If there is a chore to do, I'm more likely to",
+    a: "put it off until it needs to be done",
+    b: "get started on it right away",
   },
   {
     qno: 4,
-    qdif: "Easy",
-    q: "Who is the father of computer ?",
-    a: "Steve Jobs",
-    b: "Bill Gates",
-    c: "Charles Babbage",
-    d: "Crispin",
+    q: "I prefer to eat lunch",
+    a: "with a group of people",
+    b: "by myself",
   },
   {
     qno: 5,
-    qdif: "Easy",
-    q: "Who is the father of computer ?",
-    a: "Steve Jobs",
-    b: "Bill Gates",
-    c: "Charles Babbage",
-    d: "Crispin",
+    q: "I am patient with people, even when they aren't polite and considerate of my feelings",
+    a: "true",
+    b: "false",
+  },
+  {
+    qno: 6,
+    q: "I prefer reading rough and realistic action stories more than sensitive, imaginative novels",
+    a: "true",
+    b: "false",
+  },
+  {
+    qno: 7,
+    q: "In school I preferred (or prefer) math more than English",
+    a: "true",
+    b: "false",
+  },
+  {
+    qno: 8,
+    q: "More trouble arises from people",
+    a: "questioning and changing methods that are already satisfactory",
+    b: "turning down promising, new approaches",
+  },
+  {
+    qno: 9,
+    q: "In building or making something, I would rather work",
+    a: "with others",
+    b: "on my own",
+  },
+  {
+    qno: 10,
+    q: " I've trained myself to be patient with all kinds of people",
+    a: "true",
+    b: "false",
   },
 ];
 
@@ -76,7 +91,6 @@ const Tests = () => {
             <Card className=" col-4 w-[350px] mt-8" key={qa.qno}>
               <CardHeader>
                 <CardTitle>Question {qa.qno}</CardTitle>
-                <CardDescription>{qa.qdif}</CardDescription>
               </CardHeader>
               <CardContent>
                 <form>
@@ -85,13 +99,11 @@ const Tests = () => {
                       <Label htmlFor="name">{qa.q}</Label>
                       <Select>
                         <SelectTrigger id="name">
-                          <SelectValue placeholder="Dinesh" />
+                          <SelectValue placeholder="Select" />
                         </SelectTrigger>
                         <SelectContent position="popper">
                           <SelectItem value="1">{qa.a}</SelectItem>
                           <SelectItem value="2">{qa.b}</SelectItem>
-                          <SelectItem value="3">{qa.c}</SelectItem>
-                          <SelectItem value="4">{qa.d}</SelectItem>
                         </SelectContent>
                       </Select>
                     </div>
